c90bc850bb78ccb09ad175d85735beacdeb1fd8e
AntonioAndre03  <antonioandre1008@gmail.com>
2023-08-09 20:01:07 -0500

    Criando um layout padrão para as rotas

---
+++ rotas/src/pages/RootLayout.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Header />
+      <h1>Esse é o topo do layout padrão</h1>
+      <hr />
+      <footer>Esse é o footer do layout padrão</footer>
+    </>
+  );
+}
