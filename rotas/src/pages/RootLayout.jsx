83d01a5d0401448a75e972bbc730051693a4a467
AntonioAndre03  <antonioandre1008@gmail.com>
2023-08-09 20:13:07 -0500

    Criando um layout padrão para as rotas e Reaproveitando o layout padrão usando children

--- rotas/src/pages/RootLayout.jsx
+++ rotas/src/pages/RootLayout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 
 export default function RootLayout() {
@@ -5,6 +6,8 @@ export default function RootLayout() {
     <>
       <Header />
       <h1>Esse é o topo do layout padrão</h1>
+      <hr />
+      <Outlet />
       <hr />
       <footer>Esse é o footer do layout padrão</footer>
     </>
--- rotas/src/router.jsx
+++ rotas/src/router.jsx
@@ -2,19 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
+import RootLayout from "./pages/RootLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
   },
 ]);
 
