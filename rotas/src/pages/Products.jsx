a73d426848cb9cd57e0b2dd7a02fdebbeb3971f0
AntonioAndre03  <antonioandre1008@gmail.com>
2023-08-09 19:48:23 -0500

    Navegando entre as rotas sem atualizar a página

--- rotas/src/pages/Products.jsx
+++ rotas/src/pages/Products.jsx
@@ -1,8 +1,10 @@
+import Header from "../components/Header";
 import products from "../database.json";
 
 export default function Products() {
   return (
     <section>
+      <Header />
       <h2>Todos os produtos</h2>
       <p>Confira todas as nossas ofertas.</p>
       <section className="products">
