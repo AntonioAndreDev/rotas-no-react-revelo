bf9d9f45910bd30f2a03c137abb0e104c62a30c2
AntonioAndre03  <antonioandre1008@gmail.com>
2023-08-09 19:30:18 -0500

    Criando as páginas de visualização de cada rota

---
+++ rotas/src/pages/Cart.jsx
@@ -0,0 +1,16 @@
+export default function Cart() {
+  return (
+    <section>
+      <h2>Carrinho</h2>
+      <p>Os produtos atualmente em seu carrinho.</p>
+      <ul>
+        <li>2 un. - The Legend of Zelda: Chronicles of Hyrule</li>
+        <li>1 un. - Grand Theft Auto V</li>
+        <li>1 un. - Super Mario Odyssey</li>
+        <li>1 un. - Cosmic Conquest: Interstellar War</li>
+        <li>1 un. - Realm of Sorcery: Arcane Prophecies</li>
+      </ul>
+      <button>Finalizar compra</button>
+    </section>
+  );
+}
--- rotas/src/router.jsx
+++ rotas/src/router.jsx
@@ -1,4 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
 const router = createBrowserRouter([
   {
