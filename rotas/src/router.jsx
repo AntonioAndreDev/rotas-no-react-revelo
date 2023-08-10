b68c9a13cd4a6860b896d6c91b6284d0fb594f7d
AntonioAndre03  <antonioandre1008@gmail.com>
2023-08-09 19:01:08 -0500

    criando as rotas

---
+++ rotas/src/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+]);
+
+export default router;
