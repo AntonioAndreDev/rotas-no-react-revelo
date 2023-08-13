f14f1d3870bfa58a78e755608484fb647f20be61
AntonioAndre03  <antonioandre1008@gmail.com>
2023-08-13 17:51:30 -0500

    Renderizando os dados de cada produto de forma dinâmica:

--- rotas/src/pages/Product.jsx
+++ rotas/src/pages/Product.jsx
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import products from "../database.json";
 
 export default function Product() {
+  const { productId } = useParams();
+
+  const product = products.find((productShow) => productShow.id === +productId);
+
   return (
     <section>
       <Link to="/products">
         <button>Voltar</button>
       </Link>
-      <h1>Nome do Produto</h1>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <h4>R$ {product.price}</h4>
     </section>
   );
 }
